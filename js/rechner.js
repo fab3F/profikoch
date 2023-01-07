abd74be1455dd348bf4b755356d18e3e88ce752a
fab3F <fbnhaukede@gmail.com>
2023-01-07 03:08:13 +0100

    erstes rezept grundgestell

--- js/rechner.js
+++ js/rechner.js
@@ -2,17 +2,17 @@ let tb = document.getElementById("zutaten-tabelle").outerHTML;
 localStorage.setItem("table", tb);
 localStorage.setItem("value", document.getElementsByClassName("portionen")[0].value);
 
-function rechnen(ports){
+function calculate(ports){
 
     let orginal_table = localStorage.getItem("table");
     let orginal_value = parseInt(localStorage.getItem("value"));
 
     let temp = document.createElement('div');
     temp.innerHTML = orginal_table;
-    let orginal_rows = temp.getElementsByTagName("tr");
+    let orginal_rows = temp.getElementsByTagName("tbody")[0].getElementsByTagName("tr");
     temp.remove();
 
-    var rows = document.getElementById("zutaten-tabelle").getElementsByTagName("tr");
+    var rows = document.getElementById("zutaten-tabelle").getElementsByTagName("tbody")[0].getElementsByTagName("tr");
 
     for(let i = 0; i < orginal_rows.length; i++){
 
@@ -25,7 +25,14 @@ function rechnen(ports){
             portionen = 0;
         }
 
-        cell.innerHTML = (orginal_number / orginal_value ) * portionen;
+        if(isNumber(orginal_number)){
+            cell.innerHTML = (orginal_number / orginal_value ) * portionen;
+        }
+        
 
     }
 }
+
+function isNumber(n) {
+    return !isNaN(parseFloat(n)) && isFinite(n);
+  }
